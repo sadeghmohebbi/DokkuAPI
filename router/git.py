ed60c5004d4a3443eb6974f918341dbeb4eb4508
mohebbi <mohebbisadegh@gmail.com>
2022-03-24 13:25:18 +0430

    remove pydantic models

--- router/git.py
+++ router/git.py
@@ -3,16 +3,6 @@ from fastapi.openapi.models import APIKey
 from fastapi.responses import JSONResponse
 from commands import commands
 from router.key import validate_api_key
-from pydantic import BaseModel
-
-
-class UserInfo(BaseModel):
-    username: str = None
-    password: str = None
-
-
-class DockerImage(BaseModel):
-    name: str = None
 
 
 # Defining our API router
@@ -22,17 +12,20 @@ def get_router(app):
 
     # Authenticate a git host
     @router.post("/git/{host}/auth", response_description="Authenticate a git host")
-    async def create_app(request: Request, host: str, user_info: UserInfo, api_key: APIKey = Depends(validate_api_key)):
-        success, message = commands.git_auth(host=host, username=user_info.username, password=user_info.password)
+    async def create_app(request: Request, host: str, api_key: APIKey = Depends(validate_api_key)):
+        body_parsed = request.json()
+        success, message = commands.git_auth(host=host, username=body_parsed["username"],
+                                             password=body_parsed["password"])
         content = {"success": success, "message": message}
         return JSONResponse(status_code=status.HTTP_200_OK, content=content)
 
     # Updates an app's git repository with a given docker image
     @router.post("/git/{app_name}/from-image/{docker_image}",
                  response_description="Updates an app's git repository with a given docker image")
-    async def create_app(request: Request, app_name: str, docker_image: DockerImage,
+    async def create_app(request: Request, app_name: str,
                          api_key: APIKey = Depends(validate_api_key)):
-        success, message = commands.git_from_image(app_name=app_name, docker_image=docker_image.name)
+        body_parsed = request.json()
+        success, message = commands.git_from_image(app_name=app_name, docker_image=body_parsed["docker_image"])
         content = {"success": success, "message": message}
         return JSONResponse(status_code=status.HTTP_200_OK, content=content)
 
--- router/storage.py
+++ router/storage.py
@@ -3,12 +3,6 @@ from fastapi.openapi.models import APIKey
 from fastapi.responses import JSONResponse
 from commands import commands
 from router.key import validate_api_key
-from pydantic import BaseModel
-
-
-class StorageMount(BaseModel):
-    mount_point_left: str = None
-    mount_point_right: str = None
 
 
 # Defining our API router
@@ -17,11 +11,11 @@ def get_router(app):
     router = APIRouter()
 
     # Mount a Storage
-    @router.post("/storage/{app_name}", response_description="Authenticate a git host")
-    async def create_app(request: Request, app_name: str, storage_mount: StorageMount,
-                         api_key: APIKey = Depends(validate_api_key)):
-        success, message = commands.storage_mount(app_name=app_name, mount_point_left=storage_mount.mount_point_left,
-                                                  mount_point_right=storage_mount.mount_point_right)
+    @router.post("/storage/{app_name}", response_description="Mount a Storage")
+    async def create_app(request: Request, app_name: str, api_key: APIKey = Depends(validate_api_key)):
+        body_parsed = request.json()
+        success, message = commands.storage_mount(app_name=app_name, mount_point_left=body_parsed["mount_point_left"],
+                                                  mount_point_right=body_parsed["mount_point_right"])
         content = {"success": success, "message": message}
         return JSONResponse(status_code=status.HTTP_200_OK, content=content)
 
